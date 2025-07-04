fb666d47184aaef4aca5ffb29835082b05a3ad83
Shalopaiepta <137692721+Shalopaiepta@users.noreply.github.com>
2025-07-04 23:27:53 +0700

    Огонь по .... хутору. Буэм,домашний.

---
+++ src/com/kolesnikovroman/ExpenseDTO.java
@@ -0,0 +1,46 @@
+package com.kolesnikovroman;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ExpenseDTO {
+    private final long id;
+    private final BigDecimal amount;
+    private final LocalDate transactionDate;
+    private final String comment;
+    // Можно также добавить categoryId или categoryName, если нужно
+
+    public ExpenseDTO(long id, BigDecimal amount, LocalDate transactionDate, String comment) {
+        this.id = id;
+        this.amount = amount;
+        this.transactionDate = transactionDate;
+        this.comment = comment;
+    }
+
+    // Геттеры
+    public long getId() {
+        return id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    @Override
+    public String toString() {
+        return "ExpenseDTO{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", transactionDate=" + transactionDate +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+}
